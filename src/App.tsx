259ff16c8b8ec474447c62de7377e827327326c3
Matheus Lima Armigliato <matheus.la@hotmail.com>
2022-06-24 01:23:00 -0300

    Commit aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
+import { Event } from "./pages/Event"
 
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons{
-      id
-      title
-    }
-  }
-` 
-interface Lesson{
-  id: string,
-  title: string;
-}
 
 function App() {
- /*  MODO useEffect DE REALIZAR A REQUEST
-  useEffect(()=>{
-    client.query({
-      query: GET_LESSIONS_QUERY,
-    }).then(response =>{
-      console.log(response.data)
-    })
-  }) */
-  const { data } = useQuery<{lessons : Lesson[]}>(GET_LESSONS_QUERY)
-  
-  return (
-    <ul>
-      {data?.lessons.map(lesson =>{
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+  return(
+    <div>
+      <Event/>
+    </div>
   )
 }
 
